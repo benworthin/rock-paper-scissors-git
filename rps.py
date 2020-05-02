e92c6bcd7902d67591cf612737197e892b6bafa7
Benjamin Worthington <benworthin@icloud.com>
2020-05-02 00:13:28 -0500

    Implemented check_if_valid_input to valid any input the program requires.

--- rps.py
+++ rps.py
@@ -1,73 +1,75 @@
 import random
 
+# TODO add score functionality
 game = True
-valid_throws = ["Rock", "Paper", "Scissors"]
-valid_answers = ["Yes", "No"]
+valid_throws = ["rock", "paper", "scissors"]
+valid_answers = ["yes", "no"]
 
 
 def generate_computer_throw():
     random_num = random.randint(1, 3)
 
     if random_num == 1:
-        random_num = "Rock"
+        random_num = "rock"
     elif random_num == 2:
-        random_num = "Paper"
+        random_num = "paper"
     elif random_num == 3:
-        random_num = "Scissors"
+        random_num = "scissors"
 
     return random_num
 
 
-def check_if_valid_throw(users_throw):
-    if users_throw == "Rock" or users_throw == "Paper" or users_throw == "Scissors":
-        is_valid = True
-    else:
-        is_valid = False
+def check_if_valid_input(users_input, valid_inputs_array):
+    is_valid = False
+    for i in valid_inputs_array:
+        if users_input == i:
+            is_valid = True
 
     return is_valid
 
 
+# TODO add support for 2 players
 print("Welcome to RPS!")
 player_one = input("What is your name? > ")
-print(player_one + ", you will be playing against the computer.\n")
-
+print(f"{player_one}, you will be playing against the computer.\n")
 
 while game:
     valid_input = False
     while not valid_input:
-        player_throw = input("Do you throw Rock, Paper, or Scissors? (type exactly Rock, Paper, or Scissors.) > ")
-        valid_input = check_if_valid_throw(player_throw)
+        player_throw = input("Do you throw Rock, Paper, or Scissors? > ").lower()
+        print(player_throw)
+        valid_input = check_if_valid_input(player_throw, valid_throws)
 
         if valid_input:
             print(f"\nYou threw {player_throw}!")
         else:
             print("Invalid throw. Please try again.")
 
-    valid_input = False
-
     computer_throw = generate_computer_throw()
-    print("Computer threw " + computer_throw)
+    print(f"Computer threw {computer_throw}!")
 
-    if (player_throw == "Rock" and computer_throw == "Scissors") or (
-            player_throw == "Paper" and computer_throw == "Rock") or (
-            player_throw == "Scissors" and computer_throw == "Paper"):
+    if (player_throw == "rock" and computer_throw == "scissors") or (
+            player_throw == "paper" and computer_throw == "rock") or (
+            player_throw == "scissors" and computer_throw == "paper"):
         print(player_one + " wins! Computer loses!\n")
-    elif (player_throw == "Scissors" and computer_throw == "Rock") or (
-            player_throw == "Rock" and computer_throw == "Paper") or (
-            player_throw == "Paper" and computer_throw == "Scissors"):
+    elif (player_throw == "scissors" and computer_throw == "rock") or (
+            player_throw == "rock" and computer_throw == "paper") or (
+            player_throw == "paper" and computer_throw == "scissors"):
         print(player_one + " loses! Computer wins!\n")
     elif player_throw == computer_throw:
         print("It's a tie!\n")
 
+    # Reset variable
+    valid_input = False
     while not valid_input:
-        play_again = input("Do you want to play again? (type exactly Yes or No.) > ")
-        if play_again == "Yes":
-            game = True
-            valid_input = True
-            print("\nLet's start again!")
-        elif play_again == "No":
-            game = False
-            valid_input = True
-            print("Okay. Good-bye!")
+        play_again = input("Do you want to play again? > ").lower()
+        valid_input = check_if_valid_input(play_again, valid_answers)
+
+        if valid_input:
+            if play_again == "yes":
+                print("\nLet's start again!")
+            else:
+                game = False
+                print(f"Okay. Good-bye {player_one}!")
         else:
-            print("Please enter exactly either Yes or no.")
+            print("Invalid response. Please try again.")
